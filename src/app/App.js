53bc5e9f02410ac7556fc513e942b05e48c91867
kathydingcn <16864025@qq.com>
2018-01-24 19:46:57 +1100

    add app.js

--- src/app/App.js
+++ src/app/App.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={msg: ["msg1","msg2","msg3"]};
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+  handleAdd(e){
+    this.setState({msg: [...this.state.msg, "msg4"]});
+  }
   render() {
     return (
-      <div>Hello world</div>
+      <div>
+        <input type="text" />
+        <button onClick={this.handleAdd}>add</button>
+
+        <ul>
+            {this.state.msg.map(v=>{return <li>{v}</li>})}
+      </ul>
+      </div>
     );
   }
 }
